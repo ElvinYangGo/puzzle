bea05ee3dc6e589ec74773e8db5283f8e71d304a
Elvin <elvin@Elvin-MacBook-Pro.local>
2013-10-03 22:59:06 +0800

    modify getAdType function

--- cloud/main.js
+++ cloud/main.js
@@ -4,7 +4,19 @@ AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
+function getRand() {
+  return Math.floor((Math.random()*100)+1);
+}
+
 AV.Cloud.define("getAdType", function(request, response) {
-  response.success(1);
+  var DOMOB_YJF_RATIO = 50;
+  var rand = getRand();
+  var adType = 1;
+  if(rand < DOMOB_YJF_RATIO) {
+    adType = 1;
+  } else {
+    adType = 2;
+  }
+  response.success(adType);
 });
 
