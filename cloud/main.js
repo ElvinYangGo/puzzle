37d627a8c57385ddbec8518c82b711fc035987c2
Elvin <elvin@Elvin-MacBook-Pro.local>
2013-10-03 23:15:37 +0800

    add getInterstitialAdType and getWallAdType function

--- cloud/main.js
+++ cloud/main.js
@@ -8,15 +8,27 @@ function getRand() {
   return Math.floor((Math.random()*100)+1);
 }
 
-AV.Cloud.define("getAdType", function(request, response) {
-  var DOMOB_YJF_RATIO = 50;
+function getAdType(domobYjfRatio) {
   var rand = getRand();
   var adType = 1;
-  if(rand < DOMOB_YJF_RATIO) {
+  if(rand < domobYjfRatio) {
     adType = 1;
   } else {
     adType = 2;
   }
+  return adType;
+
+}
+
+AV.Cloud.define("getInterstitialAdType", function(request, response) {
+  var DOMOB_YJF_RATIO = 50;
+  var adType = getAdType(DOMOB_YJF_RATIO);
+  response.success(adType);
+});
+
+AV.Cloud.define("getWallAdType", function(request, response) {
+  var DOMOB_YJF_RATIO = 50;
+  var adType = getAdType(DOMOB_YJF_RATIO);
   response.success(adType);
 });
 
